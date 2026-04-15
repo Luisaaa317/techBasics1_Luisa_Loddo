81ae0bc4a49b4adeefa33b4a1622268cb03fac43
Luisaaa317 <luise.kruse@gmail.com>
2026-04-15 20:47:51 +0200

    Create assignment1.py
    This is my first assignment for tech basics 1 stream A :)

---
+++ assignments/week 1/assignment1.py
@@ -0,0 +1,34 @@
+print("""
+                      _----_        
+                     | chirp >     >(•)__
+                      -____-         |__/
+                                       ``          _----__  __---_
+                                                  |       |/      |
+                                             _-`````   _---_       `-__
+                                            |         |     |         -```-_
+                                            |                  _--_         |
+                                             ``---`           |    |   _-```
+   _                                               `-__        __---```
+  | |                                 _--_            |        |
+  | |   ___------__                 | meow |           |      |
+  | | -             - _-^--^-_     <  -__-              |    |
+  | |/               |  >  <  |                         |    |
+  -      | _____       - __ -                           |    |
+   -    / /     -       /                               |    |
+    |  |  |      |  |  |                               |      |
+    |___3__3     |___3__3                             |        |
+    
+""")
+
+
+
+
+### First I had to look up what ASCII art is since I had never heard of it before.
+### After figuring out what it was I did some brainstorming and decided on creating some animals and a tree.
+### I thought animals would be the easiest to make as someone who has no experience creating ASCII art.
+### Creating the cat and the tree was actually fairly easy, however I had some struggles with the bird.
+### Before deciding on making a bird I tried making a mouse since I thought it would work well with the cat, however I could not figure out how to make it look good.
+### I had the same struggle with the bird, since it is such a small animal, which only gave me a small space and little amount of characters to create it.
+### I'm still kinda torn on whether the bird looks good and especially whether it's recognisable, which is why I added speechbubbles with fitting animal sounds.
+
+
